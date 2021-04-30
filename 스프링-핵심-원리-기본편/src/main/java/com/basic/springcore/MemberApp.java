43070f932ed95ea647327f7f3ddb618d6bf82709
woody <woojihye2339@gmail.com>
2021-05-01 01:00:18 +0900

    feature : 메인 클래스 MemberApp 구현
    > 회원 생성 및 찾기 기능 확인
      - 회원 생성 후 해당 회원을 찾는다
      - 생성된 회원과 찾은 회원의 이름 각각 출력

---
+++ 스프링-핵심-원리-기본편/src/main/java/com/basic/springcore/MemberApp.java
@@ -0,0 +1,19 @@
+package com.basic.springcore;
+
+import com.basic.springcore.member.Grade;
+import com.basic.springcore.member.Member;
+import com.basic.springcore.member.MemberService;
+import com.basic.springcore.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.BASIC);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new findMember = " + findMember.getName());
+        System.out.println("find Member = " + member.getName());
+
+    }
+}
