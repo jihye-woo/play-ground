781696ae0898827619b9d635f82a7a62e463c683
woody <woojihye2339@gmail.com>
2021-07-05 15:38:34 +0900

    feature : AppConfig에 스프링 기능 적용
    > @Configuration 적용
      - AppConfig 클래스 레벨에 해당 어노테이션 적용
    
    > @Bean 적용
      - 각 메소드에 @Bean 어노테이션 적용

--- 스프링-핵심-원리-기본편/src/main/java/com/basic/springcore/AppConfig.java
+++ 스프링-핵심-원리-기본편/src/main/java/com/basic/springcore/AppConfig.java
@@ -1,7 +1,6 @@
 package com.basic.springcore;
 
 import com.basic.springcore.discount.DiscountPolicy;
-import com.basic.springcore.discount.FixDiscountPolicy;
 import com.basic.springcore.discount.RateDiscountPolicy;
 import com.basic.springcore.member.MemberRepository;
 import com.basic.springcore.member.MemberService;
@@ -9,24 +8,29 @@ import com.basic.springcore.member.MemberServiceImpl;
 import com.basic.springcore.member.MemoryMemberRepository;
 import com.basic.springcore.order.OrderService;
 import com.basic.springcore.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(
-                memberRepository(),
-                discountPolicy());
-    }
-
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 }
