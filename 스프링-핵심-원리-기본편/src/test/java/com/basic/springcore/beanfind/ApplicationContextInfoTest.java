e93b1cac4a23151efd6960811b576d68ea49ca75
woody <woojihye2339@gmail.com>
2021-07-05 17:40:20 +0900

    test : 역할 별로 빈을 조회하는 테스트 코드 작성
    > 빈의 역할 정보 가져오기
      - BeanDefinition 가져오기
        -> applicationContext.getBeanDefinition(빈 이름)
      - BeanDefinition를 통해 역할 가져오기
        -> beanDefinition.getRole()
        -> BeanDefinition 플래그로 역할을 구분
    
    > 어플리케이션 빈 조회
      - getRole() == ROLE_APPLICATION (직접 등록한 어플리케이션 빈)
    
    > 인프라스트럭쳐 빈 조회
      - getRole() == ROLE_INFRASTRUCTURE (스프링이 내부에서 사용하는 빈)

--- 스프링-핵심-원리-기본편/src/test/java/com/basic/springcore/beanfind/ApplicationContextInfoTest.java
+++ 스프링-핵심-원리-기본편/src/test/java/com/basic/springcore/beanfind/ApplicationContextInfoTest.java
@@ -3,6 +3,7 @@ package com.basic.springcore.beanfind;
 import com.basic.springcore.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class ApplicationContextInfoTest {
@@ -16,6 +17,39 @@ class ApplicationContextInfoTest {
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = applicationContext.getBean(beanDefinitionName);
             System.out.println("name = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("어플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = applicationContext.getBeanDefinition(beanDefinitionName);
+
+            // Role ROLE_APPLICATION : 직접 등록한 어플리케이션 빈
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                // 스프링 내부에서 등록하는 빈이 아닌
+                // 개발자가 어플리케이션 개발을 위해 등록한 빈은 ROLE_APPLICATION을 가진다.
+                Object bean = applicationContext.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("인프라스트럭쳐 빈 출력하기")
+    void findInfrastructureBean() {
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = applicationContext.getBeanDefinition(beanDefinitionName);
+
+            // Role ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_INFRASTRUCTURE) {
+                // 스프링 내부에서 등록하는 빈들만 출력
+                Object bean = applicationContext.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
         }
     }
 
