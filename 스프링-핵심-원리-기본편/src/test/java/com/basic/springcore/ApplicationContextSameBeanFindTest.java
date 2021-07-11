64a761979cea4431216a7449a473e0189277f497
woody <woojihye2339@gmail.com>
2021-07-11 11:17:52 +0900

    feature : 스프링 빈 조회 - 동일한 타입이 둘 이상
    > 같은 타입의 빈을 여러 개 등록할 수 있다
      - 이때 타입으로 조회할 경우 중복 오류가 발생할 수 있다.
      - NoUniqueBeanDefinitionException
    
    > 중족 오류 없이 빈 조회
      - 이름으로 빈 조회 (이름 + 타입)
    
    > 특정 타입을 모두 조회
      - getBeansOfType(타입 이름)
      - 스프링이 자동 주입을 할 때 내부적으로 해당 메소드를 사용

---
+++ 스프링-핵심-원리-기본편/src/test/java/com/basic/springcore/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,64 @@
+package com.basic.springcore;
+
+import com.basic.springcore.member.MemberRepository;
+import com.basic.springcore.member.MemoryMemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextSameBeanFindTest {
+
+    // SameBeanConfig 안에 있는 memberRepository1, memberRepository2을 빈으로 등록한다.
+    AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
+    void findBeanByTypeDuplicate(){
+        assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> applicationContext.getBean(MemberRepository.class));
+    }
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
+    void findBeanByName() {
+        // getBean() 메소드 호출 시, 타입과 더불어 빈 이름 "memberRepository1"을 통해 특정한 빈을 찾는다.
+        MemberRepository memberRepository = applicationContext.getBean("memberRepository1", MemberRepository.class);
+        assertThat(memberRepository).isInstanceOf(MemberRepository.class);
+    }
+
+    @Test
+    @DisplayName("특정 타입을 모두 조회하기")
+    void findBeanByType() {
+        // 왜 이런걸 알아야 하나요?
+        // @Autowired 와 같은 자동 주입을 할 때 이런 기능이 사용되기 때문에 알아놓는 것이 좋다.
+        Map<String, MemberRepository> beans = applicationContext.getBeansOfType(MemberRepository.class);
+        for(String key : beans.keySet()){
+            System.out.println("ket = " + key + " value = " + beans.get(key));
+        }
+        System.out.println("beans = " + beans);
+        assertThat(beans.size()).isEqualTo(2);
+    }
+
+
+    @Configuration
+    static class SameBeanConfig {
+
+        @Bean
+        public MemberRepository memberRepository1(){
+            return new MemoryMemberRepository();
+        }
+
+        @Bean
+        public MemberRepository memberRepository2(){
+            return new MemoryMemberRepository();
+        }
+    }
+}
